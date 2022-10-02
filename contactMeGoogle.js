0d7d941f2049cf04b8d74b2f4ed83eaea48e2209
Kağan Temizkan <ktemizkan2@gmail.com>
2022-10-02 19:47:49 +0300

    finished

--- contactMeGoogle.js
+++ contactMeGoogle.js
@@ -4,10 +4,10 @@ const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then(response => {
       msg.innerHTML = "Message sent successfully!"
-      setTimeout(function(){ msg.innerHTML = ""; }, 3000);
+      setTimeout(function () { msg.innerHTML = ""; }, 3000);
       form.reset()
     })
     .catch(error => console.error('Error!', error.message))
